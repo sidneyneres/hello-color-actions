1ae2400cc829260b226daed8e1efc1eba385fede
sidneyneres <sdn.neres@gmail.com>
2023-08-06 11:57:34 -0300

    atualizando o nome do pais

--- hello.js
+++ hello.js
@@ -1,3 +1,3 @@
-const hello = () => "Hello World from Brazil";
+const hello = () => "Hello World from Spain";
 console.log(hello());
 exports.hello = hello;
--- hello.test.js
+++ hello.test.js
@@ -1,6 +1,6 @@
 const hello = require("./hello");
 describe("My hello", () => {
   test("works", () => {
-    expect(hello.hello()).toEqual("Hello World from Brazil");
+    expect(hello.hello()).toEqual("Hello World from Spain");
   });
 });
